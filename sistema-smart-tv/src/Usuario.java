74152e638066415d698df574d73af6683430e70d
brunaborghi <brunaborghim@gmail.com>
2023-01-06 10:02:55 -0300

    atualizando

---
+++ sistema-smart-tv/src/Usuario.java
@@ -0,0 +1,29 @@
+public class Usuario {
+    public static void main(String[] args) throws Exception {
+        
+        SmartTv smartTv = new SmartTv();
+
+        System.out.println("TV Ligda? " + smartTv.ligada);
+        System.out.println("Canal Atual: " + smartTv.canal);
+        System.out.println("Volume Atual: " + smartTv.volume);
+
+
+        smartTv.ligar ();
+        System.out.println("Novo Status - TV Ligada? " + smartTv.ligada);
+    
+        smartTv.aumentarVolume();
+        System.out.println("Novo Volume: " + smartTv.volume);
+
+        smartTv.diminuirVolume();
+        System.out.println("Novo Volume: " + smartTv.volume);
+
+        smartTv.aumentarCanal();
+        System.out.println("Novo Canal Atual: " + smartTv.canal);
+        
+        smartTv.diminuirCanal();
+        System.out.println("Novo Canal Atual: " + smartTv.canal);
+
+        smartTv.mudarCanal();
+        System.out.println("Novo Canal Atual: " + smartTv.canal);
+    }
+}
